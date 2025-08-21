d706d9df23b65b46d64f61627a926ffe1c73a64a
pooja K <poojak@poojas-MacBook-Air.local>
2025-08-21 15:09:47 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",   // 👈 ensures correct routing for Vercel
   server: {
     port: 5173
   }
