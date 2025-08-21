7d5e10482dfe99482ce7e30455af237ec33d657a
pooja K <poojak@poojas-MacBook-Air.local>
2025-08-21 16:13:46 +0530

    changes for api

--- client/src/pages/AdminLogin.jsx
+++ client/src/pages/AdminLogin.jsx
@@ -19,7 +19,7 @@ const AdminLogin = ({ onLogin }) => {
     setMessage("");
 
     try {
-      const res = await axios.post(import.meta.env.VITE_API_URL+"/api/auth/login", {
+      const res = await axios.post(import.meta.env.VITE_API_URL+"/auth/login", {
         email,
         password,
       });
